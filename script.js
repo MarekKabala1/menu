9f33e8db40912efd4a56bb2089521fef5339a0fa
MarekKabala1 <83923087+MarekKabala1@users.noreply.github.com>
2022-01-21 12:24:27 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,4 @@
 const nav = document.querySelector(".head_nav-li");
-// const hamburger = document.querySelector(".head_hamburger-conteiner");
-const conteiner = document.querySelector(".head-conteiner");
-// const hamburgerLineOne = document.querySelector(".line-one ");
-// const hamburgerLineTwo = document.querySelector(".line-two ");
-// const hamburgerLineThree = document.querySelector(".line-three ");
-// hamburger.addEventListener("click", () => {
-//   nav.classList.toggle("nav-active");
-//   hamburger.classList.toggle("active");
-//   conteiner.classList.toggle("conteiner-active");
-//   hamburgerLineOne.classList.toggle("active-rotate");
-//   hamburgerLineTwo.classList.toggle("active-rotate");
-//   hamburgerLineThree.classList.toggle("active-rotate");
-// });
 const element = document.querySelector(".wrapper");
 
 element.addEventListener("click", () => {
